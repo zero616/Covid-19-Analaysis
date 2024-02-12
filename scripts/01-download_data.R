8849bf9650ee3dca2de414ef082977f3cc4059fa
Shipeng Zhang <57240985+zero616@users.noreply.github.com>
2024-02-11 19:47:53 -0500

    update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
 # Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
+# Date: 11 February 2024 [...UPDATE THIS...]
 # Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
@@ -15,12 +15,12 @@ library(tidyverse)
 
 #### Download data ####
 bullying_file_path <- 
-  "C:/Users/dell/Downloads/156901-V1/data/2021_03_30_monthly_2012_2021_state_bullying_topic.csv"
+  "./inputs/data/2021_03_30_monthly_2012_2021_state_bullying_topic.csv"
 
 bullying_raw <- read.csv(bullying_file_path)
 
 cyberbully_file_path <-
-  "C:/Users/dell/Downloads/156901-V1/data/2021_03_30_monthly_2012_2021_state_cyb_bullying_topic.csv"
+  "./inputs/data/2021_03_30_monthly_2012_2021_state_cyb_bullying_topic.csv"
 
 cyberbully_raw <- read.csv(cyberbully_file_path)
 
@@ -29,6 +29,7 @@ cyberbully_raw <- read.csv(cyberbully_file_path)
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(bullying_raw, "inputs/data/bullying_raw_data.csv") 
+write_csv(cyberbully_raw, "inputs/data/cyberbully_raw_data.csv") 
 
          
